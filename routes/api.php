bc15f88f2aadf9bbc2630c215b625afc87fb80d6
youmnalyy <youmnalyy557@gmail.com>
2026-08-13 02:59:56 +0200

    Implement average doctor rating

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,11 @@ use App\Http\Controllers\Api\DoctorController;
 use App\Http\Controllers\Api\AppointmentController;
 use App\Http\Controllers\Api\RatingController;
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
@@ -22,21 +24,30 @@ Route::prefix('auth')->group(function () {
     Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 });
 
+
 Route::middleware('auth:sanctum')->prefix('patient')->group(function () {
     Route::get('/profile', [PatientProfileController::class, 'show']);
     Route::put('/profile', [PatientProfileController::class, 'update']);
+
     Route::get('/doctors', [DoctorController::class, 'index']);
     Route::get('/doctors/{id}', [DoctorController::class, 'show']);
     Route::get('/doctors/{id}/reviews', [DoctorController::class, 'reviews']);
+
     Route::post('/appointments', [AppointmentController::class, 'store']);
     Route::get('/appointments', [AppointmentController::class, 'index']);
+
+    
     Route::post('/appointments/{id}/rating', [RatingController::class, 'store']);
 });
+
 
 Route::middleware('auth:sanctum')->prefix('doctor')->group(function () {
     Route::put('/appointments/{id}/confirm', [AppointmentController::class, 'confirm']);
     Route::put('/appointments/{id}/reject', [AppointmentController::class, 'reject']);
     Route::put('/appointments/{id}/complete', [AppointmentController::class, 'complete']);
+
+ 
+    Route::get('/{doctorId}/average-rating', [RatingController::class, 'averageRating']);
 });
 
 
